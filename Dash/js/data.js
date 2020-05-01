e64f81f4c4b70147bd9331fc5dc7bbc636979fab
henriquecarlosg <henrique.gomes@bandtec.com.br>
2020-05-01 20:10:59 -0300

    Modificações nos gráficos

--- Dash/js/data.js
+++ Dash/js/data.js
@@ -8,9 +8,26 @@ const interval = setInterval(() => {
     // Pega o horário atual
     const now = new Date();
 
-    // Formata a data conforme dd/mm/aaaa hh:ii:ss
+    // Formata a data conforme dd/mm/aaaa
     const dataHora = zeroFill(now.getUTCDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear();
+    const dataHora1 = zeroFill(now.getUTCDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear();
+    const dataHora2 = zeroFill(now.getUTCDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear();
+    const dataHora3 = zeroFill(now.getUTCDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear();
+
+    const dataHora4 = zeroFill(now.getUTCDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear();
+    const dataHora5 = zeroFill(now.getUTCDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear();
+    const dataHora6 = zeroFill(now.getUTCDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear();
+
 
     // Exibe na tela usando a div#data-hora
     document.getElementById('data-hora').innerHTML = dataHora;
+    document.getElementById('data-hora1').innerHTML = dataHora1;
+    document.getElementById('data-hora2').innerHTML = dataHora2;
+    document.getElementById('data-hora3').innerHTML = dataHora3;
+
+
+    document.getElementById('data-hora3').innerHTML = dataHora4;
+    document.getElementById('data-hora3').innerHTML = dataHora5;
+    document.getElementById('data-hora3').innerHTML = dataHora6;
+
 }, 1000);
