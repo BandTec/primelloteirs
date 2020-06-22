f2a9baf3222025fd27ef72c67789ad8a58997ca0
oliverlimabandtec <oliver.lima@bandtec.com.br>
2020-06-22 13:47:51 -0300

    adição dos formulários

--- PrimeLoitters-site/routes/alertas.js
+++ PrimeLoitters-site/routes/alertas.js
@@ -3,6 +3,11 @@ var router = express.Router();
 var sequelize = require('../models').sequelize;
 var Alerta = require('../models').Alerta;
 
+function dateToEN(date)
+{	
+	return date.split('/').reverse().join('-');
+}
+
 /* Recuperar os estabelecimentos de determinado usuario */
 router.get('/recuperar/:idEstabelecimento', function(req, res, next) {
 	
@@ -23,5 +28,33 @@ router.get('/recuperar/:idEstabelecimento', function(req, res, next) {
 			res.status(500).send(erro.message);
 	  });
 });
+
+/* Recuperar os estabelecimentos de determinado usuario para gerar um relatório*/
+router.post('/relatorio/:idEstabelecimento', function(req, res, next) {
+	
+	var idEstabelecimento = req.params.idEstabelecimento;
+	var dataInicial = req.body.de;
+	var dataFinal = req.body.ate;
+
+	var de = dateToEN(dataInicial);
+	var ate = dateToEN(dataFinal);
+	
+	console.log(`Recuperando os alertas do estabelecimento do id ${idEstabelecimento}`);
+	
+	const instrucaoSql = `select distinct a.idAlerta,a.alerta,FORMAT(a.dataAlerta,'dd/MM/yyyy') as dataAlerta,m.codigoMaquina from alerta as a, 
+						  kprMaquina as m, kprEstabelecimento as e where a.fkEstabelecimento = ${idEstabelecimento} and
+						  dataAlerta between '${de}' and '${ate}' and a.fkMaquinaAlerta = m.idMaquina;`;
+	sequelize.query(instrucaoSql, {
+		model: Alerta,
+		mapToModel: true
+	  })
+	  .then(resultado => {
+			res.json(resultado);
+	  }).catch(erro => {
+			console.error(erro);
+			res.status(500).send(erro.message);
+	  });
+});
+
 
 module.exports = router;
